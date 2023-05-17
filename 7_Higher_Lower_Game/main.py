93cf20e197485f08aa82b0dba26f0f079de1623b
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-05-17 17:09:01 +0300

    First Commit

--- 7_Higher_Lower_Game/main.py
+++ 7_Higher_Lower_Game/main.py
@@ -2,9 +2,10 @@ from art import logo
 from art import vs
 from gameData import data
 import random
+import os
 
 ######## menu ######
-def printMenu(accountA, accountB, score):    
+def printMenu(accountA, accountB, score):       
     print(logo)    
     print(f"Compare A: {accountA['name']}, a {accountA['description']}, from {accountA['country']}.")
     print(vs)
@@ -16,17 +17,20 @@ def printMenu(accountA, accountB, score):
 def compare(acA, acB, playerChoice, score):    
     if acA["follower_count"] >= acB["follower_count"] and playerChoice == "A":
         score = score + 1
+        os.system('cls') 
         print(f"\nYou' re right! Current score: {score}")
         printMenu(acB, random.choice(data), score)
     elif acA["follower_count"] <= acB["follower_count"] and playerChoice == "B":
         score = score + 1
+        os.system('cls')
         print(f"\nYou' re right! Current score: {score}")
         printMenu(acB, random.choice(data), score)
     else:
         print(f"Sorry, that's wrong. Final score: {score}")    
     
     
-def game():        
+def game(): 
+    os.system('cls')       
     accA = random.choice(data)
     accB = random.choice(data)
     startScore = 0
