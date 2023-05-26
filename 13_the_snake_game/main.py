50d22d9214b382bba0b53049a5964094a7f54252
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-05-26 16:49:10 +0300

    First Commit

--- 13_the_snake_game/main.py
+++ 13_the_snake_game/main.py
@@ -1,6 +1,8 @@
 import time
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,6 +10,9 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -20,5 +25,22 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
---
+++ 13_the_snake_game/scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+ALIGN = "center"
+FONT = ("Courier", 24, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.goto(0, 250)
+        self.score = 0
+        self.color("white")
+        self.hideturtle()
+        self.update_scoreboard()
+
+
+    def update_scoreboard(self):
+        self.write(f"Score : {self.score}", align=ALIGN, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align=ALIGN, font=FONT)
+
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
