1fcd04d840fb398db7558ec8484f8ea17d65fefb
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-05-28 20:40:49 +0300

    First Commit

--- 14_pong_game/main.py
+++ 14_pong_game/main.py
@@ -1,20 +1,52 @@
-from turtle import Turtle, Screen
+from turtle import Screen, Turtle
 from scoreboard import Scoreboard
 from center_line import Center_line
+from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("My Pong Game")
+screen.tracer(0)
 
-left_score = Scoreboard("L")
-right_score = Scoreboard("R")
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+center = Center_line()
 
-center_line = Center_line()
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
+game_is_on = True
 
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
 
+    # Detect collision with wall and bounce
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # needs to bounce
+        ball.bounce_y()
 
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
--- 14_pong_game/scoreboard.py
+++ 14_pong_game/scoreboard.py
@@ -1,21 +1,30 @@
 from turtle import Turtle
+
 ALIGN = "center"
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 80, "normal")
 
 
 class Scoreboard(Turtle):
-    def __init__(self, position):
+    def __init__(self):
         super().__init__()
-        self.penup()
-        if position == "L":
-            self.goto(-30, 250)
-        else:
-            self.goto(30, 250)
-        self.player_score = 0
         self.color("white")
+        self.penup()
         self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
         self.update_scoreboard()
 
-
     def update_scoreboard(self):
-        self.write(f"{self.player_score}", align=ALIGN, font=FONT)
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align=ALIGN, font=FONT)
+        self.goto(100, 200)
+        self.write(self.r_score, align=ALIGN, font=FONT)
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
