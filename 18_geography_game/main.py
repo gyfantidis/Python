eaccd102662d983a1ab4b6ef053c820513b7a278
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-06-05 21:49:56 +0300

    First Commit

--- 18_geography_game/main.py
+++ 18_geography_game/main.py
@@ -10,27 +10,34 @@ turtle.shape(image)
 
 # Import Data from CSV
 data = pandas.read_csv("50_states.csv")
-data_dict = data.to_dict()
-state_list = data["state"].to_list()
-print(data_dict)
+all_states = data.state.to_list()
 
-game_is_on = True
-guess = 0
+guessed_states = []
 
-while game_is_on:
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-    if state_list.index(answer_state):
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()  # make the first letter upper and the others lowers
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        print(missing_states)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
         print(answer_state.lower())
-    else:
-        pass
 
 
-    def get_mouse_click_coor(x, y):
-        print(x, y)
 
 
-    if guess >= 50:
-        game_is_on = False
-    turtle.onscreenclick(get_mouse_click_coor)
 
 turtle.mainloop()
