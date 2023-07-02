8782c75322aab1228d0e50d5c0db94a407ce474f
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-07-02 13:30:05 +0300

    First Commit

---
+++ 22_Password_manager/main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20, bg='gray75')
+
+canvas = Canvas(width=400, height=400, bg='gray75', highlightthickness=0)
+key_img = PhotoImage(file="logo.png")
+canvas.create_image(200, 200, image=key_img)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+username_label = Label(text="Email/Username:")
+username_label.grid(column=0, row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+
+
+window.mainloop()
