968b641ccbcc5ef283cb1fff31cd5766a4b671bb
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-07-05 01:03:19 +0300

    First Commit
    first Commit
    it works

---
+++ 25_Interview_Preparation/data.py
@@ -0,0 +1,212 @@
+phrases = [
+    "Get Ready for the Next Question.",
+    "Next Question Coming Up!",
+    "Brace Yourself for the Next Question.",
+    "Here Comes the Next Question.",
+    "Stay Focused for the Next Question.",
+    "Prepare Yourself for the Next Challenge.",
+    "Ready for the Next Question?",
+    "Stay Alert for the Next Question.",
+    "The Next Question Awaits.",
+    "Time for the Next Question."
+]
+
+
+interview_questions = [
+    {
+      "ID": 1,
+      "category": "Introduction",
+      "question": "Tell me about yourself."
+    },
+    {
+      "ID": 2,
+      "category": "Company Interest",
+      "question": "Why are you interested \nin working for this company?"
+    },
+    {
+      "ID": 3,
+      "category": "Education",
+      "question": "Tell me about your education."
+    },
+    {
+      "ID": 4,
+      "category": "Field of Choice",
+      "question": "Why have you chosen \nthis particular field?"
+    },
+    {
+      "ID": 5,
+      "category": "Boss Description",
+      "question": "Describe your \nbest/worst boss."
+    },
+    {
+      "ID": 6,
+      "category": "Job Interests",
+      "question": "In a job, what interests \nyou most/least?"
+    },
+    {
+      "ID": 7,
+      "category": "Weakness",
+      "question": "What is your \nmajor weakness?"
+    },
+    {
+      "ID": 8,
+      "category": "Problem Solving",
+      "question": "Give an example of \nhow you solved a problem \nin the past."
+    },
+    {
+      "ID": 9,
+      "category": "Strengths",
+      "question": "What are your strengths?"
+    },
+    {
+      "ID": 10,
+      "category": "Others' Perception",
+      "question": "How do others describe you?"
+    },
+    {
+      "ID": 11,
+      "category": "Accomplishment",
+      "question": "What do you consider \nyour best accomplishment \nin your last job?"
+    },
+    {
+      "ID": 12,
+      "category": "Future Goals",
+      "question": "Where do you see yourself \nin three years?"
+    },
+    {
+      "ID": 13,
+      "category": "Failure Analysis",
+      "question": "Think about something \nyou consider a failure in your life, \nand tell me why you \nthink it happened."
+    },
+    {
+      "ID": 14,
+      "category": "Fit into the Operation",
+      "question": "How do you think \nyou will fit into this operation?"
+    },
+    {
+      "ID": 15,
+      "category": "Contribution to Company",
+      "question": "If you were hired, \nwhat ideas/talents could you contribute \nto the position or our company?"
+    },
+    {
+      "ID": 16,
+      "category": "Leadership and Initiative",
+      "question": "Give an example where you \nshowed leadership and initiative."
+    },
+    {
+      "ID": 17,
+      "category": "Team Contribution",
+      "question": "Give an example of when \nyou were able to contribute \nto a team project."
+    },
+    {
+      "ID": 18,
+      "category": "Personal Development",
+      "question": "What have you done \nto develop or change \nin the last few years?"
+    },
+    {
+      "ID": 19,
+      "category": "Questions for Interviewer",
+      "question": "Do you have any \nquestions for me?"
+    },
+    {
+      "ID": 20,
+      "category": "Technology",
+      "question": "What programming \nlanguages are you proficient in?"
+    },
+    {
+      "ID": 21,
+      "category": "Technology",
+      "question": "Describe a challenging \ntechnical problem you encountered \nand how you resolved it."
+    },
+    {
+      "ID": 22,
+      "category": "Technology",
+      "question": "How do you stay updated \nwith the latest technology \ntrends in your field?"
+    },
+    {
+      "ID": 23,
+      "category": "Technology",
+      "question": "Explain a complex technical \nconcept to someone without \na technical background."
+    },
+    {
+      "ID": 24,
+      "category": "Technology",
+      "question": "Tell me about a project \nwhere you had to collaborate \nwith cross-functional teams."
+    },
+    {
+      "ID": 25,
+      "category": "Technology",
+      "question": "What frameworks or \nlibraries have you used in \nyour previous projects?"
+    },
+    {
+      "ID": 26,
+      "category": "Technology",
+      "question": "Describe a time when you had \nto optimize the performance \nof an application."
+    },
+    {
+      "ID": 27,
+      "category": "Technology",
+      "question": "Have you worked on any \nprojects involving cloud technologies? \nIf so, explain your experience."
+    },
+    {
+      "ID": 28,
+      "category": "Technology",
+      "question": "What steps do you follow \nto ensure the security of \nyour code and applications?"
+    },
+    {
+      "ID": 29,
+      "category": "Technology",
+      "question": "Tell me about a time when \nyou had to troubleshoot \nand debug a complex issue."
+    },
+    {
+      "ID": 30,
+      "category": "Technology",
+      "question": "How do you approach refactoring \nand improving existing codebases?"
+    },
+    {
+      "ID": 31,
+      "category": "Technology",
+      "question": "Explain the difference between \nRESTful and SOAP web services."
+    },
+    {
+      "ID": 32,
+      "category": "Technology",
+      "question": "What is your experience with \nversion control systems like Git?"
+    },
+    {
+      "ID": 33,
+      "category": "Technology",
+      "question": "Have you worked on any projects \ninvolving database management systems? \nIf so, describe your experience."
+    },
+    {
+      "ID": 34,
+      "category": "Technology",
+      "question": "How do you ensure code quality \nand maintainability in your projects?"
+    },
+    {
+      "ID": 35,
+      "category": "Technology",
+      "question": "Describe a time when you had to work \nwith legacy code. How did you approach it?"
+    },
+    {
+      "ID": 36,
+      "category": "Technology",
+      "question": "What are your preferred \ndevelopment tools and environments?"
+    },
+    {
+      "ID": 37,
+      "category": "Technology",
+      "question": "Have you worked on any projects \ninvolving mobile app development? \nIf so, explain your experience."
+    },
+    {
+      "ID": 38,
+      "category": "Technology",
+      "question": "How do you handle data security \nand privacy concerns in \nyour applications?"
+    },
+    {
+      "ID": 39,
+      "category": "Technology",
+      "question": "Tell me about your experience \nwith testing and test-driven development."
+    }
+]
+
--- 25_Interview_Preparation/main.py
+++ 25_Interview_Preparation/main.py
@@ -1,49 +1,85 @@
 import math
 from tkinter import *
+from data import interview_questions, phrases
+import random
 
 # ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
-ANSWER_MIN = 50
-SHORT_BREAK_MIN = 10
-LONG_BREAK_MIN = 30
+
+SOFT_BLUE = "#068FFF"
+HARD_BLUE = "#4E4FEB"
+WHITE = "#EEEEEE"
+FONT_NAME = "Ariel"
 reps = 0
 timer = None
 
 
-# ---------------------------- TIMER RESET ------------------------------- #
-
-def reset_timer():
-    window.after_cancel(timer)
-    canvas.itemconfig(timer_text, text="00:00")
-    head_label.config(text="Timer")
+# ---------------------------- get start -------------------#
+def get_start():
     check_label.config(text="")
-    global reps
-    reps = 0
+    get_started.grid_forget()
+    question_label.grid_forget()
+    begin_button.grid(column=1, row=9)
+    answer_sec_entry.grid(column=1, row=3)
+    prepare_sec_entry.grid(column=1, row=5)
+    questions_number_entry.grid(column=1, row=7)
+    answer_sec_entry_label.grid(column=1, row=2)
+    prepare_sec_entry_label.grid(column=1, row=4)
+    questions_number_entry_label.grid(column=1, row=6)
+    empty_label.grid(column=1, row=8)
 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- #
+# ---------------------------- begin interview ------------------------------- #
 
-def start_timer():
+def begin_interview():
+    answer_sec_entry.grid_forget()
+    prepare_sec_entry.grid_forget()
+    questions_number_entry.grid_forget()
+    answer_sec_entry_label.grid_forget()
+    prepare_sec_entry_label.grid_forget()
+    questions_number_entry_label.grid_forget()
+    empty_label.grid_forget()
+    next_button.grid(column=2, row=0)
+    end_button.grid(column=0, row=0)
+    time_label.grid(column=1, row=3)
+    category_label.grid(column=1, row=1)
+    question_label.grid(column=1, row=2)
+    begin_button.grid_forget()
+    next_question()
+
+
+# ------------------------- button next question---------------#
+def next_btn():
+    window.after_cancel(timer)
+    time_label.config(text="00:00")
+    next_question()
+
+
+# -------------------------- next question function ---------------#
+
+def next_question():
     global reps
     reps = reps + 1
-    print(reps)
-    work_sec = ANSWER_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
-    long_break_sec = LONG_BREAK_MIN * 60
+    question = random.choice(interview_questions)
+    answer_sec = int(answer_sec_entry.get())
+    short_break_sec = int(prepare_sec_entry.get())
+    number_of_questions = int(questions_number_entry.get())
 
-    if reps % 8 == 0:
-        count_down(long_break_sec)
-        head_label.config(text="Break", fg=RED)
-    elif reps % 2 == 0:
-        count_down(short_break_sec)
-        head_label.config(text="Break", fg=PINK)
+    left_questions = number_of_questions - (reps / 2)
+
+    if left_questions > 0:
+        if reps % 2 == 0:
+            category_label.config(text="")
+            count_down(short_break_sec)
+            question_label.config(text=random.choice(phrases), fg=SOFT_BLUE)
+
+        else:
+            count_down(answer_sec)
+            question_label.config(text=f"{question['question']}", fg=HARD_BLUE)
+            category_label.config(text=f"Category: {question['category']}")
+            print(f"{question['question']}")
+
     else:
-        count_down(work_sec)
-        head_label.config(text="Work", fg=GREEN)
+        end_interview()
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -53,42 +89,87 @@ def count_down(count):
     if count_sec < 10:
         count_sec = f"0{count_sec}"
 
-    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    time_label.config(text=f"{count_min}:{count_sec}")
 
     if count > 0:
         global timer
         timer = window.after(1000, count_down, count - 1)
     else:
-        start_timer()
+        next_question()
         mark = ""
         work_sessions = math.floor(reps / 2)
         for _ in range(work_sessions):
             mark += "✔️"
-        check_label.config(text=mark)
+
+        if mark:
+            check_label.config(text=f"{mark} / {int(questions_number_entry.get())}")
+        else:
+            check_label.config(text=f"{int(questions_number_entry.get())}")
+
+
+# ------------------------ end interview button ---------------------#
+def end_interview():
+    window.after_cancel(timer)
+    time_label.config(text="00:00")
+    question_label.config(text="Interview Completed. \nClick Begin Interview for a New Session!")
+    category_label.config(text="")
+    next_button.grid_forget()
+    end_button.grid_forget()
+    time_label.grid_forget()
+    get_started.grid(column=1, row=5)
+    global reps
+    reps = 0
 
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Interview Preparation")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=100, pady=50, bg=WHITE)
 
-head_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50, "bold"))
-head_label.grid(column=1, row=0)
+# ---------------- LABELS ---------------------#
 
-check_label = Label(bg=YELLOW, fg=GREEN)
-check_label.grid(column=1, row=3)
+category_label = Label(text="", bg=WHITE, fg="black", font=(FONT_NAME, 20, "bold"))
 
-start_button = Button(text="Start", highlightthickness=0, command=start_timer)
-start_button.grid(column=0, row=2)
+question_label = Label(text="Get Ready for Your Interview", bg=WHITE, fg=HARD_BLUE, font=(FONT_NAME, 40, "bold"))
+question_label.grid(column=1, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
-reset_button.grid(column=2, row=2)
+time_label = Label(text="00:00", bg=WHITE, fg="red", font=(FONT_NAME, 35, "bold"))
 
-canvas = Canvas(width=500, height=500, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="interview2.png")
-canvas.create_image(250, 250, image=tomato_img)
-timer_text = canvas.create_text(250, 50, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(column=1, row=1)
+check_label = Label(bg=WHITE, fg=HARD_BLUE, font=(FONT_NAME, 15, "bold"))
+check_label.grid(column=1, row=6)
+
+# ------------------ BUTTONS ----------------------#
+
+get_started = Button(bg=SOFT_BLUE, fg=WHITE, font=(FONT_NAME, 35, "bold"), text="Get Started",
+                     highlightthickness=0,
+                     command=get_start)
+get_started.grid(column=1, row=5)
+
+begin_button = Button(bg=SOFT_BLUE, fg=WHITE, font=(FONT_NAME, 35, "bold"), text="Begin Interview",
+                      highlightthickness=0,
+                      command=begin_interview)
+
+next_button = Button(text="Next \nQuestion", font=(FONT_NAME, 35, "bold"), fg=SOFT_BLUE, highlightthickness=0,
+                     command=next_btn)
+end_button = Button(text="Finish \nInterview", font=(FONT_NAME, 35, "bold"), fg=SOFT_BLUE, highlightthickness=0,
+                    command=end_interview)
+
+# ------------------ ENTRYS --------------------------#
+
+answer_sec_entry = Entry(width=60)
+prepare_sec_entry = Entry(width=60)
+questions_number_entry = Entry(width=60)
+answer_sec_entry_label = Label(width=60, text="Answer Time (sec):")
+prepare_sec_entry_label = Label(width=60, text="Prepare Time (sec):")
+questions_number_entry_label = Label(width=60, text="Number of Questions:")
+empty_label = Label(width=60, text="")
+
+# -------------------- CANVAS -------------------------------#
+
+canvas = Canvas(width=500, height=500, bg=WHITE, highlightthickness=0)
+interview_img = PhotoImage(file="images/interview2.png")
+canvas.create_image(250, 250, image=interview_img)
+canvas.grid(column=1, row=0)
 
 window.mainloop()
